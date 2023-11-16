a5534e5bda48935bf7dd8c0f0ae751069d3dae17
judismar <solid.judis@gmail.com>
2023-11-16 10:20:25 -0300

    Add files via upload

--- despesas.sql
+++ despesas.sql
@@ -1,5 +1,5 @@
 --
--- Arquivo gerado com SQLiteStudio v3.4.4 em seg nov 13 16:37:03 2023
+-- Arquivo gerado com SQLiteStudio v3.4.4 em qui nov 16 10:18:15 2023
 --
 -- Codificação de texto usada: UTF-8
 --
@@ -8,15 +8,51 @@ BEGIN TRANSACTION;
 
 -- Tabela: despesa
 CREATE TABLE IF NOT EXISTS despesa (id_despesa INTEGER PRIMARY KEY, nomed TEXT);
+INSERT INTO despesa (id_despesa, nomed) VALUES (1, 'outubro');
+INSERT INTO despesa (id_despesa, nomed) VALUES (2, 'novembro');
+INSERT INTO despesa (id_despesa, nomed) VALUES (3, 'dezembro');
 
 -- Tabela: gasto
 CREATE TABLE IF NOT EXISTS gasto (id_gasto INTEGER PRIMARY KEY, nomeg TEXT NOT NULL, valorg NUMERIC NOT NULL, id_despesa INTEGER REFERENCES despesa (id_despesa), datag INTEGER);
+INSERT INTO gasto (id_gasto, nomeg, valorg, id_despesa, datag) VALUES (1, 'compras', 132.98, 1, NULL);
+INSERT INTO gasto (id_gasto, nomeg, valorg, id_despesa, datag) VALUES (2, 'compras', 52.34, 1, NULL);
+INSERT INTO gasto (id_gasto, nomeg, valorg, id_despesa, datag) VALUES (3, 'compras', 243.55, 2, NULL);
+INSERT INTO gasto (id_gasto, nomeg, valorg, id_despesa, datag) VALUES (4, 'compras', 49.05, 3, NULL);
+INSERT INTO gasto (id_gasto, nomeg, valorg, id_despesa, datag) VALUES (5, 'compras', 97.1, 3, NULL);
+INSERT INTO gasto (id_gasto, nomeg, valorg, id_despesa, datag) VALUES (6, 'PS5', 3599.99, 3, NULL);
+INSERT INTO gasto (id_gasto, nomeg, valorg, id_despesa, datag) VALUES (7, 'bichos', 146, 2, NULL);
+INSERT INTO gasto (id_gasto, nomeg, valorg, id_despesa, datag) VALUES (8, 'petiscos p/ bichos', 12.3, 1, NULL);
+INSERT INTO gasto (id_gasto, nomeg, valorg, id_despesa, datag) VALUES (9, 'petiscos p/ bichos', 23, 1, NULL);
+INSERT INTO gasto (id_gasto, nomeg, valorg, id_despesa, datag) VALUES (10, 'petiscos p/ bichos', 35.7, 2, NULL);
+INSERT INTO gasto (id_gasto, nomeg, valorg, id_despesa, datag) VALUES (11, 'petiscos p/ bichos', 10.3, 3, NULL);
+INSERT INTO gasto (id_gasto, nomeg, valorg, id_despesa, datag) VALUES (12, 'petiscos p/ bichos', 50, 1, NULL);
+INSERT INTO gasto (id_gasto, nomeg, valorg, id_despesa, datag) VALUES (13, 'lanche', 3.5, 1, NULL);
+INSERT INTO gasto (id_gasto, nomeg, valorg, id_despesa, datag) VALUES (14, 'lanche', 25, 1, NULL);
+INSERT INTO gasto (id_gasto, nomeg, valorg, id_despesa, datag) VALUES (15, 'lanche', 1.5, 1, NULL);
+INSERT INTO gasto (id_gasto, nomeg, valorg, id_despesa, datag) VALUES (16, 'lanche', 5.1, 2, NULL);
+INSERT INTO gasto (id_gasto, nomeg, valorg, id_despesa, datag) VALUES (17, 'lanche', 21.35, 2, NULL);
+INSERT INTO gasto (id_gasto, nomeg, valorg, id_despesa, datag) VALUES (18, 'lanche', 1, 3, NULL);
+INSERT INTO gasto (id_gasto, nomeg, valorg, id_despesa, datag) VALUES (19, 'lanche', 5, 3, NULL);
+INSERT INTO gasto (id_gasto, nomeg, valorg, id_despesa, datag) VALUES (20, 'corte de cabelo', 30, 2, NULL);
+INSERT INTO gasto (id_gasto, nomeg, valorg, id_despesa, datag) VALUES (21, 'dívida com João', 100, 1, NULL);
+INSERT INTO gasto (id_gasto, nomeg, valorg, id_despesa, datag) VALUES (22, 'Dívida com João', 20, 2, NULL);
 
 -- Tabela: GastoFixo
-CREATE TABLE IF NOT EXISTS GastoFixo (id_gasto_fixo INTEGER PRIMARY KEY UNIQUE NOT NULL, nomegf TEXT NOT NULL, valorgf REAL NOT NULL, datagf INTEGER);
+CREATE TABLE IF NOT EXISTS GastoFixo (id_gastofixo INTEGER PRIMARY KEY UNIQUE NOT NULL, nomegf TEXT NOT NULL, valorgf REAL NOT NULL, datagf INTEGER);
+INSERT INTO GastoFixo (id_gastofixo, nomegf, valorgf, datagf) VALUES (1, 'internet', 89.99, NULL);
+INSERT INTO GastoFixo (id_gastofixo, nomegf, valorgf, datagf) VALUES (2, 'condomínio', 200.0, NULL);
+INSERT INTO GastoFixo (id_gastofixo, nomegf, valorgf, datagf) VALUES (3, 'netflix', 19.99, NULL);
 
 -- Tabela: tem
-CREATE TABLE IF NOT EXISTS tem (id_despesa INTEGER REFERENCES despesa (id_despesa), id_gasto_fixo INTEGER REFERENCES GastoFixo (id_gasto_fixo), PRIMARY KEY (id_despesa, id_gasto_fixo)) WITHOUT ROWID;
+CREATE TABLE IF NOT EXISTS tem (id_despesa INTEGER REFERENCES despesa (id_despesa), id_gastofixo INTEGER REFERENCES GastoFixo (id_gastofixo), PRIMARY KEY (id_despesa, id_gastofixo)) WITHOUT ROWID;
+INSERT INTO tem (id_despesa, id_gastofixo) VALUES (1, 1);
+INSERT INTO tem (id_despesa, id_gastofixo) VALUES (1, 2);
+INSERT INTO tem (id_despesa, id_gastofixo) VALUES (2, 1);
+INSERT INTO tem (id_despesa, id_gastofixo) VALUES (2, 2);
+INSERT INTO tem (id_despesa, id_gastofixo) VALUES (2, 3);
+INSERT INTO tem (id_despesa, id_gastofixo) VALUES (3, 1);
+INSERT INTO tem (id_despesa, id_gastofixo) VALUES (3, 2);
+INSERT INTO tem (id_despesa, id_gastofixo) VALUES (3, 3);
 
 COMMIT TRANSACTION;
 PRAGMA foreign_keys = on;
--- loja-roupas.sql
+++ loja-roupas.sql
@@ -1,5 +1,5 @@
 --
--- Arquivo gerado com SQLiteStudio v3.4.4 em seg nov 13 16:34:39 2023
+-- Arquivo gerado com SQLiteStudio v3.4.4 em qui nov 16 10:19:44 2023
 --
 -- Codificação de texto usada: UTF-8
 --
@@ -8,15 +8,49 @@ BEGIN TRANSACTION;
 
 -- Tabela: cargo
 CREATE TABLE IF NOT EXISTS cargo (id_cargo INTEGER PRIMARY KEY, descricao TEXT, salario REAL NOT NULL);
+INSERT INTO cargo (id_cargo, descricao, salario) VALUES (1, 'Auxiliar de Atendente', 1100.0);
+INSERT INTO cargo (id_cargo, descricao, salario) VALUES (2, 'Atendente', 2310.3);
+INSERT INTO cargo (id_cargo, descricao, salario) VALUES (3, 'Gerente', 12314.5);
 
 -- Tabela: cliente
 CREATE TABLE IF NOT EXISTS cliente (id_cliente INTEGER PRIMARY KEY, nomec TEXT NOT NULL, id_funcionario INTEGER REFERENCES funcionario (id_funcionario));
+INSERT INTO cliente (id_cliente, nomec, id_funcionario) VALUES (1, 'Beatriz', 7);
+INSERT INTO cliente (id_cliente, nomec, id_funcionario) VALUES (2, 'José', 1);
+INSERT INTO cliente (id_cliente, nomec, id_funcionario) VALUES (3, 'Beto', 1);
+INSERT INTO cliente (id_cliente, nomec, id_funcionario) VALUES (4, 'Dolores', 2);
+INSERT INTO cliente (id_cliente, nomec, id_funcionario) VALUES (5, 'Ana', 1);
+INSERT INTO cliente (id_cliente, nomec, id_funcionario) VALUES (6, 'Maria', 2);
+INSERT INTO cliente (id_cliente, nomec, id_funcionario) VALUES (7, 'Karol', 3);
+INSERT INTO cliente (id_cliente, nomec, id_funcionario) VALUES (8, 'Orisvaldo', 6);
+INSERT INTO cliente (id_cliente, nomec, id_funcionario) VALUES (9, 'Beth', 5);
+INSERT INTO cliente (id_cliente, nomec, id_funcionario) VALUES (10, 'Mateus', 4);
+INSERT INTO cliente (id_cliente, nomec, id_funcionario) VALUES (11, 'João', 4);
+INSERT INTO cliente (id_cliente, nomec, id_funcionario) VALUES (12, 'Adalberto', 5);
 
 -- Tabela: funcionario
 CREATE TABLE IF NOT EXISTS funcionario (id_funcionario INTEGER PRIMARY KEY, nomef TEXT NOT NULL, id_cargo INTEGER REFERENCES cargo (id_cargo));
+INSERT INTO funcionario (id_funcionario, nomef, id_cargo) VALUES (1, 'Ednéia', 3);
+INSERT INTO funcionario (id_funcionario, nomef, id_cargo) VALUES (2, 'Marlene', 3);
+INSERT INTO funcionario (id_funcionario, nomef, id_cargo) VALUES (3, 'Kelly', 3);
+INSERT INTO funcionario (id_funcionario, nomef, id_cargo) VALUES (4, 'Patrícia', 2);
+INSERT INTO funcionario (id_funcionario, nomef, id_cargo) VALUES (5, 'Valéria', 2);
+INSERT INTO funcionario (id_funcionario, nomef, id_cargo) VALUES (6, 'Adilson', 2);
+INSERT INTO funcionario (id_funcionario, nomef, id_cargo) VALUES (7, 'Dante', 1);
 
 -- Tabela: roupa
 CREATE TABLE IF NOT EXISTS roupa (id_roupa INTEGER PRIMARY KEY, tipo TEXT, valor REAL NOT NULL, id_funcionario INTEGER REFERENCES funcionario (id_funcionario));
+INSERT INTO roupa (id_roupa, tipo, valor, id_funcionario) VALUES (1, 'bermuda', 49.99, 1);
+INSERT INTO roupa (id_roupa, tipo, valor, id_funcionario) VALUES (2, 'camisa', 79.9, 1);
+INSERT INTO roupa (id_roupa, tipo, valor, id_funcionario) VALUES (3, 'blusa', 99.9, 1);
+INSERT INTO roupa (id_roupa, tipo, valor, id_funcionario) VALUES (4, 'bermuda', 29.99, 2);
+INSERT INTO roupa (id_roupa, tipo, valor, id_funcionario) VALUES (5, 'casaco', 119.9, 2);
+INSERT INTO roupa (id_roupa, tipo, valor, id_funcionario) VALUES (6, 'vestido', 249.99, 3);
+INSERT INTO roupa (id_roupa, tipo, valor, id_funcionario) VALUES (7, 'calça', 49.99, 4);
+INSERT INTO roupa (id_roupa, tipo, valor, id_funcionario) VALUES (8, 'bermuda', 19.99, 4);
+INSERT INTO roupa (id_roupa, tipo, valor, id_funcionario) VALUES (9, 'calça', 69.99, 5);
+INSERT INTO roupa (id_roupa, tipo, valor, id_funcionario) VALUES (10, 'blusa', 35.5, 6);
+INSERT INTO roupa (id_roupa, tipo, valor, id_funcionario) VALUES (11, 'blusa', 999.99, 7);
+INSERT INTO roupa (id_roupa, tipo, valor, id_funcionario) VALUES (12, 'vestido', 139.9, 5);
 
 COMMIT TRANSACTION;
 PRAGMA foreign_keys = on;
